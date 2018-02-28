785babfcb961122e36125e2e69a7879e70303e12
Marc Walter <walter.marc@outlook.com>
2018-02-28 22:07:55 +0100

    More levels

--- src/Main.elm
+++ src/Main.elm
@@ -98,10 +98,22 @@ loadLevel index model =
     in
     case index of
         0 ->
-            level [ 0, 3, 1, 3 ] 3 3
+            level [ 0, 1, 2, 1, 0 ] 3 3
 
         1 ->
-            level [ 1, 2, 0, 1, 0, 1, 3 ] 3 3.5
+            level [ 1, 2, 0, 1, 0, 1 ] 3 3.5
+
+        2 ->
+            level [ 2, 3, 1, 2, 0, 0, 3 ] 3 3.5
+
+        3 ->
+            level [ 0, 1, 3, 0, 4, 4, 3, 4 ] 2.5 4
+
+        4 ->
+            level [ 1, 2, 3, 2, 0, 1, 0, 2 ] 2.5 5
+
+        5 ->
+            level [ 4, 2, 3, 3, 1, 0, 4, 1 ] 2.5 5
 
         _ ->
             { model_ | stage = GameOver }
